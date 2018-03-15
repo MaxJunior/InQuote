f2ef33bc636af5ece57c2540455127ae8da9aba7
MaxJunior <maxwell989@gmail.com>
2018-03-15 18:29:16 +0000

    Add initial folders and files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//region {variables and functions}
+var quoteId = "quote";
+var quoteContent ;
+var 
+var setBackgroundColorQuery;
+var quoteAuthor;
+
+function getQuoteContent(){
+	quoteContent = document.getElementById(quoteId);
+}
+
+function getNewQuote(){
+
+}
+
+function setNewColorSqueme(){
+
+}
+
+function setQuoteContent(){
+ 
+}
+// end-region
+
+//region {calls}
+
+//region
