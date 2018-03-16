a38333147e06f2bf4f8387b9ecbf7ef92ac7bbcb
MaxJunior <maxwell989@gmail.com>
2018-03-16 17:35:15 +0000

    Implement logics functions.Horay its awesome.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,79 @@
 //region {variables and functions}
 var quoteId = "quote";
-var quoteContent ;
-var 
-var setBackgroundColorQuery;
+var newQuoteButtonId  ="newQuoteButton";
+var backgroundColorId ="backgroundColor";
+var rgbString = "rgb";
 var quoteAuthor;
+var backgroundColorIndex = 0;
+var quoteIndex = 0;
 
-function getQuoteContent(){
-	quoteContent = document.getElementById(quoteId);
+var backgroundColorList =["(155,89,182)","(119,177,169)","(115,168,87)","(71,46,50)",
+                            "(205,220,57)","(255,64,129)"];
+var quotesObject = [ //{"authorName":"Leslie Brown", "shortName":"Les Brown",
+					// "quote":
+					 "Its not over until I win."
+                     //},
+                    // {"authorName":"Winston Churchill", "shortName":"Do not know"
+                    ,
+					// "quote":
+					 "If you'are going through hell, keep going."
+                   //  },
+                   //  {"authorName":"People", "shortName":"Pips"
+                   ,
+					// "quote":
+					 "If its not broken dont fix it."
+                   //  }
+                   , 
+                    "Difficulties mastered are opportunities won."
+                   ];
+
+function getContent(contentId)
+{
+	var content = document.getElementById(contentId);
+    return content;
 }
 
-function getNewQuote(){
-
+function getNewQuote(currentQuoteIndex)
+{
+	quoteIndex = (currentQuoteIndex == (quotesObject.length - 1)) ? 0 : currentQuoteIndex + 1 ; 
+    return  quotesObject[quoteIndex];
 }
 
-function setNewColorSqueme(){
-
+function getNewBackgroundColor(currentBackgroundColorIndex)
+{
+    backgroundColorIndex = (currentBackgroundColorIndex == (backgroundColorList.length - 1)) ? 0 : currentBackgroundColorIndex + 1;
+    return backgroundColorList[backgroundColorIndex];
 }
 
-function setQuoteContent(){
- 
+function setNewColorSqueme(backgroundColorId,currentBackgroundColorIndex)
+{
+   var pageContent = getContent(backgroundColorId);
+   var buttonIdContent = getContent(newQuoteButtonId);
+  //setBackgroundColorQuery += getNewBackgroundColor(currentBackgroundColorIndex) + '}');
+  pageContent.style.background = rgbString + getNewBackgroundColor(currentBackgroundColorIndex) ;
+  pageContent.style.color = rgbString + getNewBackgroundColor(currentBackgroundColorIndex) ;
+  buttonIdContent.style.background = rgbString + getNewBackgroundColor(currentBackgroundColorIndex) ;
+}
+
+function setQuoteContent(quoteId,currentQuoteIndex)
+{
+   var quoteContent = getContent(quoteId);
+   quoteContent.innerHTML = getNewQuote(currentQuoteIndex);
+}
+
+function changePageContent(backgroundColorId,currentBackgroundColorIndex,quoteId,currentQuoteIndex)
+{
+	setNewColorSqueme(backgroundColorId,currentBackgroundColorIndex);
+	setQuoteContent(quoteId,currentQuoteIndex);
 }
 // end-region
 
+
 //region {calls}
+//console.log(document.getElementsByTagName("body").innerHTML);
+
+//document.addEventListener('click', changePageContent(backgroundColorId,backgroundColorIndex,quoteId,quoteIndex));
+  
+
 
 //region
